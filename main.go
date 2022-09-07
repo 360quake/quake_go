cbf8626d0c92b36f9f2278fcf3b2d8c1940db0eb
rootphantomer <rootphantomy@hotmail.com>
2022-09-07 15:01:57 +0800

    fix up

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
  * @Author: ph4nt0mer
  * @Date: 2022-08-31 17:03:03
  * @LastEditors: rootphantomer
- * @LastEditTime: 2022-09-07 14:54:55
+ * @LastEditTime: 2022-09-07 15:01:50
  * @FilePath: /quake_go/main.go
  * @Description:
  *
@@ -27,93 +27,6 @@ func main() {
 	hflag_init()
 }
 
-//	func Init() {
-//		fmt.Println("Starting Quake Cli...")
-//		num := len(os.Args)
-//		if num < 2 {
-//			fmt.Println(`
-//
-// Usage of ./quake:
-//
-//	  init <token>
-//			token initialization
-//	  info
-//	        person infomation
-//	  query <string>
-//	        query string value
-//	  -t <string>
-//	        field string value(example:./quake query port:8088 -t=body,)
-//	  -ic bool
-//	        ignore_cache value (true or false)
-//	  -s time
-//			start time value, USE UTC
-//	  -e time
-//			end time value, USE UTC
-//	  -start string
-//	        start String value (default :./quake query port:8088 -start=0)
-//	  -size string
-//	        size String value (default :./quake query port:8088 -size=10)`)
-//			return
-//		}
-//		path := "./config.yaml"
-//		if strings.ToLower(os.Args[1]) == "init" {
-//			if num < 3 {
-//				fmt.Println("!!!!token is empty !!!!")
-//				return
-//			}
-//			utils.WriteYaml(path, os.Args[2])
-//			return
-//		}
-//		token, status := utils.ReadYaml(path)
-//		if status {
-//			fmt.Println("!!!!please ./quake init token!!!!")
-//			return
-//		}
-//		var reqjson Reqjson
-//		reqjson.Field = "ip,port"
-//		for _, value := range os.Args {
-//			tmp := strings.Split(value, "=")
-//			if strings.Contains(tmp[0], "-start") {
-//				// reqjson.Start = tmp[1]
-//			}
-//			if strings.Contains(tmp[0], "-size") {
-//				// reqjson.Size = tmp[1]
-//			}
-//			if strings.Contains(tmp[0], "-t") {
-//				// reqjson.Field = tmp[1]
-//			}
-//			if strings.Contains(tmp[0], "-s") {
-//				// reqjson.Start_time = tmp[1]
-//			}
-//			if strings.Contains(tmp[0], "-e") {
-//				// reqjson.End_time = tmp[1]
-//			}
-//			if strings.Contains(tmp[0], "-ic") {
-//				if strings.Contains(tmp[1], "true") {
-//					reqjson.Ignore_cache = true
-//				}
-//			}
-//		}
-//		switch strings.ToLower(os.Args[1]) {
-//		case "info":
-//			apis.InfoGet(token.Token)
-//		case "query":
-//			if num < 3 {
-//				fmt.Println("!!!!query is empty !!!!")
-//				return
-//			}
-//			reqjson.Query = os.Args[2]
-//			apis.SearchServicePost(reqjson, token.Token)
-//		case "host":
-//			fmt.Println("主机数据接口待完成。。。")
-//		case "favicon":
-//			fmt.Println("favicon相似度接口待完成。。。")
-//		case "domain":
-//			fmt.Println("domain ")
-//		default:
-//			fmt.Println("args failed !!!!")
-//		}
-//	}
 func hflag_init() {
 	fmt.Println("Starting Quake Cli...")
 	hflag.AddFlag("start", "start flag", hflag.Shorthand("st"), hflag.Type("string"), hflag.DefaultValue("0"))
