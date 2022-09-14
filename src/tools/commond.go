0655b45a57024077d88b1c0983f6ed427f558425
rootphantomer <rootphantomy@hotmail.com>
2022-09-14 12:09:42 +0800

    合并重复代码

--- src/tools/commond.go
+++ src/tools/commond.go
@@ -2,7 +2,7 @@
  * @Author: ph4nt0mer
  * @Date: 2022-09-01 10:49:31
  * @LastEditors: rootphantomer
- * @LastEditTime: 2022-09-07 15:47:47
+ * @LastEditTime: 2022-09-14 12:09:32
  * @FilePath: /quake_go/src/tools/commond.go
  * @Description:
  *
@@ -18,38 +18,16 @@ import (
 	"strings"
 )
 
-func ApisPost(url string, data []byte, token string) string {
+func Apis(url string, method string, data []byte, token string) string {
 	client := &http.Client{}
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
-	if err != nil {
-		fmt.Println(err)
-		return err.Error()
+	var req *http.Request
+	var err error
+	switch method {
+	case "POST":
+		req, err = http.NewRequest("POST", url, bytes.NewBuffer(data))
+	case "GET":
+		req, err = http.NewRequest("GET", url, nil)
 	}
-	req.Header.Add("X-QuakeToken", token)
-	req.Header.Add("Content-Type", "application/json")
-
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		return err.Error()
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println(err)
-		return err.Error()
-	}
-	fmt.Println("result:")
-	// fmt.Println(string(body))
-	if strings.Contains(string(body), "quake/login") {
-		fmt.Println("token expired,please init new token")
-	}
-	return string(body)
-}
-func ApisGet(url string, token string) string {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		fmt.Println(err)
 		return err.Error()
